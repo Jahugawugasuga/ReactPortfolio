33eb1faf7e06cba01feb665817569d8e67cf5bff
James Brooks <jamesb91@gmail.com>
2020-07-26 20:30:38 -0600

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,29 @@ class Contact extends Component {
     render() {
         return (
             <div>
-                some contact text
+                <div class="card border-light mb-3">
+        <div id="h1_secondary">Contact</div>
+        <hr/>
+        <div class="card-body">
+          <form>
+            <div class="form-group">
+              <label for="exampleFormControlInput1">Name</label>
+              <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="John Smith"/>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Email Address</label>
+                <input type="name" class="form-control" placeholder="name@example.com"/>
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Email functionality Coming Soon!" rows="4"></textarea>
+              </div>
+              <input id="btnbtnbtn" type="submit" value="Submit"/>
+          
+        </div>
+        </form>
+
+      </div>
+    </div>
             </div>
 
         )
