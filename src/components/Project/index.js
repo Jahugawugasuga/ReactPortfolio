65da8d2733e04a145b4d9c7b2f807c7d78d583e1
James Brooks <jamesb91@gmail.com>
2020-07-27 01:11:49 -0600

    added project component, yarn

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./style.css";
+// import vault from "../Img/jason-dent-3wPJxh-piRw-unsplash.jpg"
+// import team from "../Img/bekir-donmez-eofm5R5f9Kw-unsplash.jpg"
+// import lightning from "../Img/max-larochelle-uu-Jw5SunYI-unsplash.jpg"
+// import planner from "../Img/stil-flRm0z3MEoA-unsplash.jpg"
 
-function Project() {
+function Project(props) {
+
+
     return (
         <div>
-          im the Project
+            <div className="imgcontainer">
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-4 img-responsive">
+                        <img id="password" className="img" src={props.image}
+                            alt="vault" />
+                        <div>
+                            <p id="linkfont">
+                                <a href={props.link}>{props.id}{props.title}</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
