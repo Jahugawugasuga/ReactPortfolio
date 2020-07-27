e2caf2aa18b95c2bf461fbbe9b5a89b77e34bb4f
James Brooks <jamesb91@gmail.com>
2020-07-26 20:55:42 -0600

    about and header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,17 @@ import "./style.css";
 function Footer() {
     return (
         <div>
-          im the footer
+         <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4><a id="footer" href="https://linkedin.com/in/ejamesbrooks"> LinkedIn Page</a></h4>
+                    <h4><a id="footer" href="https://github.com/Jahugawugasuga">Github Page</a></h4>
+                    <h4><a id="footer" href="mailto:jamesb91@gmail.com">Email</a></h4>
+                    <h4><a id="footer" href="https://www.linkedin.com/in/ejamesbrooks/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAABImF4gBlZ8qqCvo-wa92Q0x99XXazfdcUE,1592887950839)/">Resume</a></h4>
+
+                </div>
+            </div>
+        </footer>
         </div>
     );
 };
