501d705e0b63bb33a6e0fb6d0ef53aaaf882110a
James Brooks <jamesb91@gmail.com>
2020-07-27 21:56:42 -0600

    fixed images

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -9,21 +9,21 @@ function Project(props) {
 
 
     return (
-        <div>
-            <div className="imgcontainer">
-                <div className="row">
-                    <div className="col-lg-4 col-md-4 col-sm-4 img-responsive">
-                        <img id="password" className="img" src={props.image}
-                            alt="vault" />
-                        <div>
-                            <p id="linkfont">
-                                <a href={props.link}>{props.id}{props.title}</a>
-                            </p>
-                        </div>
+
+        <div className="imgcontainer">
+            <div className="row">
+                <div className="col-lg-4 col-md-4 col-sm-4 img-responsive">
+                    <img id="password" className="img" src={require("../../Img/" + props.image)}
+                        alt="vault" />
+                    <div>
+                        <p id="linkfont">
+                            <a href={props.link}>{props.id}{props.title}</a>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
