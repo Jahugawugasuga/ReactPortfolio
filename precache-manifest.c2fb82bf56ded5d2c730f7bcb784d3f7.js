a3dfabc5b61696ef0e87ce65ecf393e5244decf0
James Brooks <jamesb91@gmail.com>
2020-07-27 19:47:15 -0600

    Updates

---
+++ precache-manifest.c2fb82bf56ded5d2c730f7bcb784d3f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321812d0925017b4bef14c51b374dcc8",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "2607aaf270b30e6bf3fa",
+    "url": "/reactportfolio/static/css/main.72514313.chunk.css"
+  },
+  {
+    "revision": "f9ea917800b558a383a4",
+    "url": "/reactportfolio/static/js/2.984408fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.984408fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2607aaf270b30e6bf3fa",
+    "url": "/reactportfolio/static/js/main.a4f8d68b.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "1bda758acc9a651847bd662f882f12fa",
+    "url": "/reactportfolio/static/media/brooksfam.1bda758a.png"
+  }
+]);
